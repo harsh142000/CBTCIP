eb7ff6b9e04cd31644e88f32e3a4545d92ddc7a0
Harsh Raj <harshmishra142000@gmail.com>
2023-07-25 23:10:59 +0530

    Delete AtmMachineProject directory

--- AtmMachineProject/src/AtmOperationInterf.java
+++
@@ -1,9 +0,0 @@
-
-public interface AtmOperationInterf {
-    public void viewBalance();
-    public void withdrawAmount(double withdrawAmount);
-    public void depositAmount(double depositAmount );
-    public  void viewMiniStatement();
-
-    public void transferAmount(String recipientAccountNumber,double transfer);
-}
