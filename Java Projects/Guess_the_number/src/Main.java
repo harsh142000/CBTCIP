04833946efbe550715e62811df5d52e83dc418ea
Harsh Raj <harshmishra142000@gmail.com>
2023-07-26 18:11:41 +0530

    Update Main.java

--- Java Projects/Guess_the_number/src/Main.java
+++ Java Projects/Guess_the_number/src/Main.java
@@ -3,74 +3,76 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        // Welcome message
-        JOptionPane.showMessageDialog(null, "Welcome to Guess the Number Game!");
-
-        // Variables
-        int rangeMin = 1;
-        int rangeMax = 100;
-        int maxAttempts = 10;
-        int currentScore = 0;
-        int rounds = 1;
-
-        while (true) {
-            // Generate a random number
-            int randomNumber = generateRandomNumber(rangeMin, rangeMax);
-
-            // Initialize variables for the current round
-            int attempts = 0;
-            boolean guessed = false;
-
-            // Play the current round
-            while (attempts < maxAttempts && !guessed) {
-                // Prompt user to guess the number
-                String input = JOptionPane.showInputDialog("Round " + rounds + ": Guess the number between " + rangeMin + " and " + rangeMax);
-
-                // Check if the input is a valid integer
-                int guess;
-                try {
-                    guess = Integer.parseInt(input);
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.");
-                    continue;
-                }
-
-                // Compare the user's guess with the random number
-                if (guess < randomNumber) {
-                    JOptionPane.showMessageDialog(null, "Higher! Try again.");
-                } else if (guess > randomNumber) {
-                    JOptionPane.showMessageDialog(null, "Lower! Try again.");
-                } else {
-                    // Guessed correctly
-                    JOptionPane.showMessageDialog(null, "Congratulations! You guessed the number in " + (attempts + 1) + " attempts.");
-                    guessed = true;
-                    currentScore += maxAttempts - attempts;
-                }
-
-                attempts++;
-            }
-
-            // Check if the user has reached the maximum number of attempts
-            if (!guessed) {
-                JOptionPane.showMessageDialog(null, "Out of attempts! The number was: " + randomNumber);
-            }
-
-            // Ask if the user wants to play another round
-            int option = JOptionPane.showConfirmDialog(null, "Your current score: " + currentScore + "\nDo you want to play another round?", "Play Again?", JOptionPane.YES_NO_OPTION);
-            if (option == JOptionPane.NO_OPTION) {
-                break;
-            }
-
-            rounds++;
-        }
-
-        // Game Over
-        JOptionPane.showMessageDialog(null, "Game Over! Your final score: " + currentScore);
-    }
-
-    // Helper method to generate a random number within the specified range
-    private static int generateRandomNumber(int min, int max) {
         Random random = new Random();
-        return random.nextInt(max - min + 1) + min;
+        int maxAttempts = 10;
+        int rangeStart = 1;
+        int rangeEnd = 100;
+        int score = 0;
+
+        // Show a welcome message using a dialogue box
+        JOptionPane.showMessageDialog(null, "Welcome to Guess the Number game!");
+
+        do {
+            // Generate a random number within the specified range
+            int targetNumber = random.nextInt(rangeEnd - rangeStart + 1) + rangeStart;
+
+            // Ask the user to guess the number using a dialogue box
+            String input = JOptionPane.showInputDialog("Guess a number between " + rangeStart + " and " + rangeEnd + ".");
+            int attempts = 0;
+
+            if (input == null) {
+                break; // User clicked the cancel button, exit the game loop
+            }
+
+            int guess = Integer.parseInt(input);
+
+            while (attempts < maxAttempts) {
+                attempts++;
+
+                if (guess == targetNumber) {
+                    // Show a success message if the guessed number is correct
+                    JOptionPane.showMessageDialog(null, "Congratulations! You guessed the correct number!");
+
+                    // Calculate points earned and update the total score
+                    int points = maxAttempts - attempts + 1;
+                    score=points;
+
+                    // Show points earned in the current round using a dialogue box
+                    JOptionPane.showMessageDialog(null, "Points earned: " + points);
+                    break;
+                } else if (guess < targetNumber) {
+                    // Ask the user for a higher number since their guess was too low
+                    input = JOptionPane.showInputDialog("Your guess is too low. Enter a higher number:");
+                } else {
+                    // Ask the user for a lower number since their guess was too high
+                    input = JOptionPane.showInputDialog("Your guess is too high. Enter a lower number:");
+                }
+
+                if (input == null) {
+                    break; // User clicked the cancel button, exit the game loop
+                }
+
+                guess = Integer.parseInt(input);
+            }
+
+            if (attempts == maxAttempts) {
+                // Show a message when the user runs out of attempts
+                JOptionPane.showMessageDialog(null, "Sorry, you have used all your attempts. The correct number was: " + targetNumber);
+                score=0;
+            }
+
+            // Ask the user if they want to play another round
+            int continueOption = JOptionPane.showConfirmDialog(null, "Do you want to play another round?", "Continue?", JOptionPane.YES_NO_OPTION);
+
+            if (continueOption == JOptionPane.NO_OPTION) {
+                // Show a message when the user decides to exit the game
+                JOptionPane.showMessageDialog(null, "Game Over");
+                break; // Exit the game loop
+            }
+
+        } while (true);
+
+        // Show the final score using a dialogue box when the user exits the game
+        JOptionPane.showMessageDialog(null, "Your final score is: " + score);
     }
 }
